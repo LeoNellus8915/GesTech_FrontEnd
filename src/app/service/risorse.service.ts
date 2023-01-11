259cec207478bfdae4d8803a14a6127487d972d1
RiccardoBarionovi <riccardo.barionovi@rpctech.it>
2023-01-11 11:22:46 +0100

    Grafici e statistica

--- src/app/service/risorse.service.ts
+++ src/app/service/risorse.service.ts
@@ -60,4 +60,24 @@ export class RisorseService {
   public getCandidatiByRecruiterDate(idRecruiter: number, changeForm: JSON): Observable<any> {
     return this.http.post<any>(`${this.apiServerUrl}/get-candidati-by-recruiter-date/${idRecruiter}`, changeForm, {headers: header.header()});
   }
+
+  public getCandidatiTotali(): Observable<any> {
+    return this.http.get<any>(`${this.apiServerUrl}/get-candidati-totali`, {headers: header.header()});
+  }
+
+  public getCandidatiGiornalieri(): Observable<any> {
+    return this.http.get<any>(`${this.apiServerUrl}/get-candidati-giornalieri`, {headers: header.header()});
+  }
+
+  public getPercentualiRuoli(): Observable<any> {
+    return this.http.get<any>(`${this.apiServerUrl}/get-percentuali-ruoli`, {headers: header.header()});
+  }
+
+  public countCandidatiGiornalieriByRecruiter(): Observable<any> {
+    return this.http.get<any>(`${this.apiServerUrl}/get-candidati-giornalieri-by-recriter`, {headers: header.header()});
+  }
+
+  public countCandidatiTotaliByRecruiter(): Observable<any> {
+    return this.http.get<any>(`${this.apiServerUrl}/get-candidati-totali-by-recriter`, {headers: header.header()});
+  }
 }
--- src/app/templates/pages/registroAttivita/registro-attivita.component.ts
+++ src/app/templates/pages/registroAttivita/registro-attivita.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { CandidatiService } from 'src/app/service/candidati.service';
+import { ProfiliService } from 'src/app/service/profili.service';
 import { RisorseService } from 'src/app/service/risorse.service';
 import { DefaultComponent } from '../../default/default.component';
+import {Chart} from 'chart.js/auto';
 
 @Component({
   templateUrl: './registro-attivita.component.html',
@@ -13,8 +16,18 @@ export class PaginaRegistroAttivita implements OnInit{
   public ruolo: string = sessionStorage.getItem("ruolo") as string;
   public titoloPagina: any;
   public listaRecruiters!: any[];
+  public numeroCandidatiTotali!: number;
+  public numeroCandidatiGiornalieri!: number;
+  public listaProfili!: any;
 
-  constructor(private router: Router, private titleService: Title, private defaultService: DefaultComponent, private risorseService: RisorseService) {}
+  public listaPercentuali = new Array();
+  public listaNumeroCandidatiGiornalieriRecruiter = new Array();
+  public listaNumeroCandidatiTotaliRecruiter = new Array();
+
+  public chart: any;
+
+  constructor(private router: Router, private titleService: Title, private defaultService: DefaultComponent, private risorseService: RisorseService, 
+    private candidatiService: CandidatiService, private profiliService: ProfiliService) {}
 
   ngOnInit(): void {
     if (this.ruolo == null)
@@ -26,6 +39,10 @@ export class PaginaRegistroAttivita implements OnInit{
           this.defaultService.titoloPagina=" Registro Attività";
         }, 0)
         this.getRecruiters();
+        this.candidatiTotali();
+        this.candidatiGiornalieri();
+        this.getProfili();
+        this.getPercentualiRuoli();
       }
       else{
         this.router.navigate(["default/pagina-avvisi"]);
@@ -35,7 +52,99 @@ export class PaginaRegistroAttivita implements OnInit{
   getRecruiters() {
     this.risorseService.getRecruiters().subscribe(
       (response: any) => {
-        this.listaRecruiters = response.dataSource;
+        if (response.codeSession == "0") {
+          sessionStorage.setItem("sessionMessage", "Sessione scaduta");
+          this.defaultService.logout();
+        }
+        else {
+          this.listaRecruiters = response.dataSource;
+          let recruiters = new Array();
+      
+          response.dataSource.forEach((recruiter: any) => {
+            recruiters.push(recruiter.nome + " " + recruiter.cognome[0] + ".");
+          });
+
+          this.risorseService.countCandidatiGiornalieriByRecruiter().subscribe(
+            (response: any) => {
+              response.dataSource.forEach((numero: any) => {
+                this.listaNumeroCandidatiGiornalieriRecruiter.push(numero);
+              });
+
+              this.risorseService.countCandidatiTotaliByRecruiter().subscribe(
+                (response: any) => {
+                  response.dataSource.forEach((numero: any) => {
+                    this.listaNumeroCandidatiTotaliRecruiter.push(numero);
+                  });
+                  this.chart = new Chart("MyChart", {
+                    type: 'bar',
+                    data: {
+                      labels: recruiters, 
+                        datasets: [
+                        {
+                          label: "Candidati Totali",
+                          data: this.listaNumeroCandidatiTotaliRecruiter,
+                          backgroundColor: 'green'
+                        },
+                        {
+                          label: "Candidati Giornalieri",
+                          data: this.listaNumeroCandidatiGiornalieriRecruiter,
+                          backgroundColor: 'orange'
+                        }  
+                      ]
+                    },
+                    options: {
+                      aspectRatio: 1.5
+                    }
+                  });
+                }
+              )
+            }
+          )
+        }
+      }
+    )
+  }
+
+  getProfili() {
+    this.profiliService.getProfili().subscribe(
+      (response: any) => {
+        if (response.codeSession == "0") {
+          sessionStorage.setItem("sessionMessage", "Sessione scaduta");
+          this.defaultService.logout();
+        }
+        else {
+          this.listaProfili = response.dataSource
+        }
+      }
+    )
+  }
+
+  getPercentualiRuoli() {
+    this.risorseService.getPercentualiRuoli().subscribe(
+      (response: any) => {
+        var totale = 0;
+        response.dataSource.forEach((profilo: number) => {
+          totale = totale + profilo;
+        });
+        this.listaProfili.forEach((profilo: any) => {
+          this.listaPercentuali.push({"nomeProfilo": profilo.nome, "percentuale": Math.floor((response.dataSource[profilo.id-1] * 100) / totale)})
+        });
+      }
+    )
+  }
+
+  candidatiTotali() {
+    this.risorseService.getCandidatiTotali().subscribe(
+      (response: any) => {
+        this.numeroCandidatiTotali = response.dataSource;
+      }
+    )
+  }
+
+  candidatiGiornalieri() {
+    this.risorseService.getCandidatiGiornalieri().subscribe(
+      (response: any) => {
+        this.numeroCandidatiGiornalieri = response.dataSource;
       }
     )
   }
